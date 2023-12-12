8c885460cd0df9a6b4f1ff66e28532b2903ea891
Eugen <nikelodiym1210@gmail.com>
2023-12-12 14:45:47 +0200

    show&close modal window

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+const modalWindow = document.querySelector('.modal-window');
+const modalBtns = document.querySelectorAll('.show-modal-window');
+const closeModalBtn = document.querySelector('.close-modal-window');
+const overlay = document.querySelector('.overlay');
+
+const showModalWindow = function () {
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+const closeModal = function () {
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < modalBtns.length; i++){
+        modalBtns[i].addEventListener('click', showModalWindow)
+}
+
+closeModalBtn.addEventListener('click', closeModal)
+
+overlay.addEventListener('click', (e) => {
+if (e.target !== modalWindow){
+    closeModal();
+    } 
+})
